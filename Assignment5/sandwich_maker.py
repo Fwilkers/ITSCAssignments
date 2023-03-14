223bb4c5e5d077d967c556f64b8f46b159eeec6a
KaijuMittens <93461965+KaijuMittens@users.noreply.github.com>
2023-03-13 20:02:37 -0400

    Initial Commit

---
+++ Assignment5/sandwich_maker.py
@@ -0,0 +1,143 @@
+from flask import Flask, flash, redirect, url_for, render_template, session
+from flask import request
+
+from flask_sqlalchemy import SQLAlchemy
+
+DB_HOST = "localhost"
+DB_NAME = "sandwichmaker"
+DB_USERNAME = "root"
+DB_Password = "root"
+
+database_file = f"mysql+pymysql://{DB_USERNAME}:{DB_Password}@{DB_HOST}:3306/{DB_NAME}"
+
+app = Flask(__name__)
+app.secret_key = "mysecret"
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+
+db = SQLAlchemy(app)
+
+
+class Resource(db.Model):
+    __tablename__ = 'resources'
+    id = db.Column(db.Integer, primary_key=True)
+    item = db.Column(db.String(100), nullable=False)
+    amount = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, item, amount):
+        self.item = item
+        self.amount = amount
+
+
+class Sandwiches(db.Model):
+    __tablename__ = 'sandwiches'
+    id = db.Column(db.Integer, primary_key=True)
+    sandwich_size = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+    def __init__(self, sandwich_size, price):
+        self.sandwich_size = sandwich_size
+        self.price = price
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/resource")
+def resource():
+    return render_template("resources/list.html", resources=Resource.query.all())
+
+
+@app.route("/sandwiches")
+def sandwich():
+    return render_template("sandwiches/list.html", sandwiches=Sandwiches.query.all())
+
+
+@app.route('/addresource', methods=['GET', 'POST'])
+def add_resource():
+    if request.method == 'POST':
+        if not request.form['item'] or not request.form['amount']:
+            flash('Please enter all the fields', 'error')
+        else:
+            resource = Resource(request.form['item'], request.form['amount'])
+            db.session.add(resource)
+            db.session.commit()
+
+            flash('Record was successfully added')
+            return redirect(url_for('resource'))
+    return render_template('resources/add.html')
+
+@app.route('/addsandwich', methods=['GET', 'POST'])
+def add_sandwich():
+    if request.method == 'POST':
+        if not request.form['sandwich_size'] or not request.form['price']:
+            flash('Please enter all the fields', 'error')
+        else:
+            sandwich = Sandwiches(request.form['sandwich_size'], request.form['price'])
+            db.session.add(sandwich)
+            db.session.commit()
+
+            flash('Record was successfully added')
+            return redirect(url_for('sandwich'))
+    return render_template('sandwiches/add.html')
+
+@app.route('/updateresource/<int:id>/', methods=['GET', 'POST'])
+def update_resource(id):
+    if request.method == 'POST':
+        if not request.form['item'] or not request.form['amount']:
+            flash('Please enter all the fields', 'error')
+        else:
+            resource = Resource.query.filter_by(id=id).first()
+            resource.item = request.form['item']
+            resource.amount = request.form['amount']
+            db.session.commit()
+
+            flash('Record was successfully updated')
+            return redirect(url_for('resource'))
+    data = Resource.query.filter_by(id=id).first()
+    return render_template("resources/update.html", data=data)
+
+@app.route('/updatesandwich/<int:id>/', methods=['GET', 'POST'])
+def update_sandwich(id):
+    if request.method == 'POST':
+        if not request.form['sandwich_size'] or not request.form['price']:
+            flash('Please enter all the fields', 'error')
+        else:
+            sandwich = Sandwiches.query.filter_by(id=id).first()
+            sandwich.sandwich_size = request.form['sandwich_size']
+            sandwich.price = request.form['price']
+            db.session.commit()
+
+            flash('Record was successfully updated')
+            return redirect(url_for('sandwich'))
+    data = Sandwiches.query.filter_by(id=id).first()
+    return render_template("sandwiches/update.html", data=data)
+
+
+@app.route('/deleteresource/<int:id>/', methods=['GET', 'POST'])
+def delete_resource(id):
+    if request.method == 'POST':
+        resource = Resource.query.filter_by(id=id).first()
+        db.session.delete(resource)
+        db.session.commit()
+
+        flash('Record was successfully deleted')
+        return redirect(url_for('resource'))
+    data = Resource.query.filter_by(id=id).first()
+    return render_template("resources/delete.html", data=data)
+
+@app.route('/deletesandwich/<int:id>/', methods=['GET', 'POST'])
+def delete_sandwich(id):
+    if request.method == 'POST':
+        sandwich = Sandwiches.query.filter_by(id=id).first()
+        db.session.delete(sandwich)
+        db.session.commit()
+
+        flash('Record was successfully deleted')
+        return redirect(url_for('sandwich'))
+    data = Sandwiches.query.filter_by(id=id).first()
+    return render_template("sandwiches/delete.html", data=data)
+
+if __name__ == '__main__':
+    app.run(port=3001, host="localhost", debug=True)
---
+++ Assignment5/sql_scripts/tables.sql
@@ -0,0 +1,16 @@
+-- resources Table:
+CREATE TABLE IF NOT EXISTS resources (
+    id int NOT NULL AUTO_INCREMENT,
+    item varchar(50) NOT NULL,
+    amount int NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- sandwiches table:
+
+CREATE TABLE IF NOT EXISTS sandwiches (
+    id int NOT NULL AUTO_INCREMENT,
+    sandwich_size varchar(50) NOT NULL,
+    price decimal (5,2) NOT NULL,
+    PRIMARY KEY (id)
+);
